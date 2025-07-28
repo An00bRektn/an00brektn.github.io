af458af395cb506ae0f96acc7dba9370f5ace3ff
An00bRektn <An00bRektn@users.noreply.github.com>
2025-07-28 11:31:02 -0500

    chore: bump version to 1.6.4
    untested locally, just praying I copied good :)

--- src/consts.ts
+++ src/consts.ts
@@ -1,35 +1,24 @@
-export type Site = {
-  TITLE: string
-  DESCRIPTION: string
-  EMAIL: string
-  NUM_POSTS_ON_HOMEPAGE: number
-  POSTS_PER_PAGE: number
-  SITEURL: string
-}
-
-export type Link = {
-  href: string
-  label: string
-}
+import type { IconMap, SocialLink, Site } from '@/types'
 
 export const SITE: Site = {
-  TITLE: 'root@nayyyr:~/blog#',
-  DESCRIPTION:
+  title: 'root@nayyyr:~/blog#',
+  description:
     'Not another infosec/computer science blog.',
-  EMAIL: 'An00bRektn@proton.me',
-  NUM_POSTS_ON_HOMEPAGE: 2,
-  POSTS_PER_PAGE: 3,
-  SITEURL: 'https://an00brektn.github.io',
+  author: 'an00b',
+  locale: 'en-US',
+  featuredPostCount: 2,
+  postsPerPage: 3,
+  href: 'https://an00brektn.github.io',
 }
 
-export const NAV_LINKS: Link[] = [
+export const NAV_LINKS: SocialLink[] = [
   { href: '/blog', label: 'blog' },
   { href: '/authors', label: 'authors' },
   { href: '/about', label: 'about' },
   { href: '/tags', label: 'tags' },
 ]
 
-export const SOCIAL_LINKS: Link[] = [
+export const SOCIAL_LINKS: SocialLink[] = [
   { href: 'https://github.com/An00bRektn', label: 'GitHub' },
   { href: 'https://twitter.com/An00bRektn', label: 'Twitter' },
   { href: 'An00bRektn@proton.me', label: 'Email' },
